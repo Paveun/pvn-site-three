4d95c4ec62203c279e6482e0067408810aef4e5b
Paveun <a.tresierrajansen@gmail.com>
2023-04-13 21:42:47 -0400

    initial commit

---
+++ randomtext.js
@@ -0,0 +1,28 @@
+var r_text = new Array ();
+r_text[0] = "Are you supposed to be here?";
+r_text[1] = "Makes computers go brr.";
+r_text[2] = "Working hard to hardly work.";
+r_text[3] = "Capable of answering skill testing questions.";
+r_text[4] = "Is definitely not a robot.";
+r_text[5] = "When life gives you melons, you might be dyslexic.";
+r_text[6] = "Don’t you hate it when someone answers their own questions? I do.";
+r_text[7] = "Most people are shocked when they find out how bad I am as an electrician.";
+r_text[8] = "Russian dolls are so full of themselves.";
+r_text[9] = "People who use selfie sticks really need to have a good, long look at themselves.";
+r_text[10] = "Atheism is a non-prophet organization.";
+r_text[11] = "I got a new pair of gloves today, but they’re both ‘lefts,’ which on the one hand is great, but on the other, it’s just not right.";
+r_text[12] = "I didn’t think orthopedic shoes would help, but I stand corrected.";
+r_text[13] = "Despite the high cost of living, it remains popular.";
+r_text[14] = "The rotation of Earth really makes my day.";
+r_text[15] = "Well, to be Frank with you, I’d have to change my name.";
+r_text[16] = "Are people born with photographic memories, or does it take time to develop?";
+r_text[17] = "Communist jokes aren’t funny unless everyone gets them.";
+r_text[18] = "The first time I got a universal remote control, I thought to myself, ‘This changes everything.’";
+r_text[19] = "Whiteboards are remarkable.";
+r_text[20] = "The man who survived both mustard gas and pepper spray is a seasoned veteran now.";
+r_text[21] = "Refusing to go to the gym is a form of resistance training.";
+r_text[22] = "Never trust atoms; they make up everything";
+
+var i = Math.floor(r_text.length * Math.random());
+
+document.write(r_text[i]);
